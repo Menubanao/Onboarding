3392388794352d606468c54385541dca95848161
UTTAM KUMAR <rajuttam4439@gmail.com>
2026-03-02 16:47:24 +0530

    Error Solving

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    server: {
+        port: 5500, // Matching the Live Server port they are used to
+        open: true
+    }
+});
